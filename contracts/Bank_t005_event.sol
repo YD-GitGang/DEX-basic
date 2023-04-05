1600e1217d022be49946b2599aed58a294115522
YD-GitGang <b.maca217@gmail.com>
2023-04-05 10:01:33 +0900

    commit_t006

---
+++ contracts/Bank_t005_event.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+
+contract Bank {
+
+    event balanceUpdate(string _txType, address _owner, uint _amount);
+
+    mapping(address => uint)balance;
+    
+    function getbalance() public view returns(uint){
+        return balance[msg.sender];
+    }
+
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+        emit balanceUpdate("Deposit", msg.sender, balance[msg.sender]);
+    }
+
+    function withdraw(uint _amount) public {
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+        balance[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit balanceUpdate("Withdraw", msg.sender, balance[msg.sender]);
+    }
+
+    function transfer(address _to, uint _amount) public {
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+        require(msg.sender != _to, "Invalid recipient");
+        balance[msg.sender] -= _amount;
+        balance[_to] += _amount;
+        emit balanceUpdate("Outgoing Transfer", msg.sender, balance[msg.sender]);
+        emit balanceUpdate("Incoming Transfer", _to, balance[_to]);
+    }
+}
